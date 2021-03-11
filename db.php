c4d06a6211399b3b48000833793606a6b2e1c7d0
Nongharu1210 <6306021422194@fitm.kmutnb.ac.th>
2021-03-11 14:50:03 +0700

    update

--- db.php
+++ db.php
@@ -11,3 +11,29 @@
     else echo "Connect success..";
    
 ?>
+
+<?php
+        //แสดงข้อมูลใน table
+        $sql = "SELECT * FROM `book`";
+        $result = $mysqli->query($sql);
+        echo "<table border='1'>";
+        $counter = 0;
+        while ($row = $result->fetch_assoc()) {
+            if ($counter == 0) {
+                echo "<tr>";
+                foreach ($row as $key => $value) {
+                    echo "<th>{$key}</th>";
+                }
+                
+                echo "</tr>";
+                $counter++;
+            }
+            echo "<tr>";
+            foreach ($row as $key => $value) {
+                echo "<td>{$value}</td>";
+            }
+            
+            echo "</tr>";
+        }
+        echo "</table>";
+        ?>
