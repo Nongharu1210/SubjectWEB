2a5a81029a66ff7e553972f3ad1e946c2e3d91a9
Nongharu1210 <6306021422194@fitm.kmutnb.ac.th>
2021-03-11 14:35:49 +0700

    update

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
     define("hostname","localhost");
-    define("username",'user');
-    define("password","ec1L9W4hnfSNcQia");
+    define("username",'root');
+    define("password","");
     define("dbname","bookstore");
 ?>
 <?php
     $conn=new mysqli(hostname,username,password,dbname);
     $conn->query("SELECT * FROM `book` WHERE 1");
     if($conn->connect_error) echo "not connect";
-    else echo "Connect success";
+    else echo "Connect success..";
    
 ?>
